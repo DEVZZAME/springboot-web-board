50b4c7d91169d8b9c666441b838715a856884921
DEVZZAME <92978598+DEVZZAME@users.noreply.github.com>
2023-01-26 01:02:41 +0900

    Revert "#1 - Spring Boot 버전 down grade"
    This reverts commit b4fc28a1d8a65d74d057690444ea7842cd1d08f6.

--- src/main/java/com/hansol/webboard/domain/Article.java
+++ src/main/java/com/hansol/webboard/domain/Article.java
@@ -1,13 +1,16 @@
 package com.hansol.webboard.domain;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
+import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
